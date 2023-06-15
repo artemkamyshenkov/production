e22f567b01bdcc780be887045de04413a5ebfa12
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-06-15 23:00:48 +0300

    add i18n

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,11 +2,13 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import styles from './Navbar.module.scss';
 import { AppLink } from 'shared/ui/AppLink';
 import { AppLickTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
   className?: string;
 }
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t, i18n } = useTranslation();
   return (
     <div className={classNames(styles.navbar)}>
       <div className={classNames(styles.links)}>
@@ -15,10 +17,10 @@ export const Navbar = ({ className }: NavbarProps) => {
           className={classNames(styles.mainLink, {}, [className])}
           theme={AppLickTheme.SECONDARY}
         >
-          Главная
+          {t('Главная')}
         </AppLink>
         <AppLink to={'/about'} theme={AppLickTheme.SECONDARY}>
-          О сайте
+          {t('О сайте')}
         </AppLink>
       </div>
     </div>
